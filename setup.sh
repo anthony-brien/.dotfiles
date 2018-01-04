4c4d139097501a8dc1e4b03936705635990abd64
Anthony Brien <anthony.brien@ubisoft.com>
2018-01-04 01:20:55 -0500

    Simplify bash setup script

--- setup.sh
+++ setup.sh
@@ -2,30 +2,30 @@
 
 # Sets up symbolic links in the correct locations.
 
+function SetupLink()
+{
+
+    #echo ln -f "$1" "$2"
+    ln -f "$1" "$2"
+
+    echo $1 =\> $2
+}
+
 # Hyper terminal
-rm ~/.hyper.js
-ln ~/.dotfiles/hyperterminal/.hyper.js ~/.hyper.js 
+SetupLink ~/.dotfiles/hyperterminal/.hyper.js ~/.hyper.js 
 
 # Visual Studio Code
-rm ~/Library/Application\ Support/Code/User/settings.json
-ln ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json 
+SetupLink ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 # bash
-rm ~/.bash_profile
-ln ~/.dotfiles/bash/.bash_profile ~/.bash_profile
-rm ~/.bashrc
-ln ~/.dotfiles/bash/.bashrc ~/.bashrc
-rm ~/.powerline-shell.json
-ln ~/.dotfiles/bash/.powerline-shell.json ~/.powerline-shell.json
+SetupLink ~/.dotfiles/bash/.bash_profile ~/.bash_profile
+SetupLink ~/.dotfiles/bash/.bashrc ~/.bashrc
+SetupLink ~/.dotfiles/bash/.powerline-shell.json ~/.powerline-shell.json
 
 # zsh
-rm ~/.zshrc
-ln ~/.dotfiles/zsh/.zshrc ~/.zshrc
-rm ~/.oh-my-zsh
-ln ~/.dotfiles/zsh/.zshrc ~/.zshrc
+SetupLink ~/.dotfiles/zsh/.zshrc ~/.zshrc
+SetupLink ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
 # Git
-rm ~/.gitconfig
-ln ~/.dotfiles/git/.gitconfig ~/.gitconfig
-rm ~/.gitignore
-ln ~/.dotfiles/git/.gitignore ~/.gitignore
+SetupLink ~/.dotfiles/git/.gitconfig ~/.gitconfig
+SetupLink ~/.dotfiles/git/.gitignore ~/.gitignore
