b67d07ad3f9b23dfaf8e60893d324aa7f17226f4
Unknown <anthony.brien@ubisoft.com>
2018-01-21 21:06:02 -0500

    Add Zlocation to powershell

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,16 @@
 # Powershell for current user and for all shells
 Import-Module posh-git
 Import-Module oh-my-posh
+Import-Module ZLocation
 Set-Theme paradox
+
+If ($IsMacOS -eq 'True') 
+{
+    # redefine the default oh-my-posh prompt as it causes issues on MacOS
+    function prompt {
+        "`n" + $(Get-Location) + " " + $(Write-VcsStatus) + " "
+    }
+}
 
 Import-Module Get-ChildItemColor
  
