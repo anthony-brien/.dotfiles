3b9ee2e2006b9116c1348da7c5d054876b1acdd2
Unknown <anthony.brien@ubisoft.com>
2018-01-26 18:09:57 -0500

    Remove redundant aliases

--- cmder/config/user-aliases.cmd
+++ cmder/config/user-aliases.cmd
@@ -5,7 +5,6 @@
 ;= goto:eof
 ;= Add aliases below here
 e.=explorer .
-gl=git log --oneline --all --graph --decorate  $*
 ls=ls --show-control-chars -F --color $*
 pwd=cd
 clear=cls
@@ -14,14 +13,6 @@ unalias=alias /d $1
 vi=vim $*
 cmderr=cd /d "%CMDER_ROOT%"
 e.=explorer .
-gl=git log --oneline --all --graph --decorate  $*
-pwd=cd
-clear=cls
-history=cat %CMDER_ROOT%\config\.history
-unalias=alias /d $1
-ls=ls --show-control-chars --group -F --color $*  
-cdpl=f: $t cd f:\anvil-playground\$1
-cdmain=d: $t cd d:\main\$1
 vs2017="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" $*
 vs2015="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" $*
 
