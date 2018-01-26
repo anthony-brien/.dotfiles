a9c78eee69ace820546110701032fa2bdac59f08
Unknown <anthony.brien@ubisoft.com>
2018-01-26 18:12:07 -0500

    powershell load custom p4 functions
    And dont auto-load Jump.location module as if its loaded in vscode and cmder at the same time, they obliterate each others history

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,8 @@
 # Powershell for current user and for all shells
 Import-Module posh-git
 Import-Module oh-my-posh
-Import-Module Jump.Location
+# VSCode auto loads powershell, which wipes the jumpstat, so don't autoload for now
+# Import-Module Jump.Location 
 
 Set-Theme paradox
 
@@ -19,8 +20,19 @@ If ($IsMacOS -eq 'True')
     }
 }
 
+function mklink ($target, $link)
+{
+    New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
 Import-Module Get-ChildItemColor
  
 # TODO -option AllScope prob needs to be renamed to -Force
 Set-Alias l Get-ChildItemColor -option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+
+# Perforce aliases
+#TODO Set-Alias Get-LatestIntegratedChangelist "p4 changes -s submitted -m1"
+
+# Load perforce helper functions
+. ~/powershell-scripts/p4-functions.ps1
