72e6945410d2c1100fc0030ebbee20954a365ec6
Anthony Brien <anthony.brien@ubisoft.com>
2019-11-20 22:43:32 +0000

    Updated kraken, rider and gmaster launch cmds

---
+++ windows/cmds/gmaster.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+start "" C:\Users\abrien\AppData\Local\gmaster\bin\gmaster.exe --path=%*
--- windows/cmds/kraken.cmd
+++ windows/cmds/kraken.cmd
@@ -1,3 +1,5 @@
 @echo off
 
-start "" "C:\Users\abrien\AppData\Local\gitkraken\app-6.1.4\gitkraken.exe" -p %*
+start "" "C:\Users\abrien\AppData\Local\gitkraken\app-6.3.1\gitkraken.exe" -p %*
+
+REM start "" C:\Users\abrien\AppData\Local\gitkraken\update.exe --process-start-args="-p %*"
